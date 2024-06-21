2a2176b0426fe14b27951170dea06973e926cec8
Christopher Lewis Pinedo Paredes <150399841+Pastemc@users.noreply.github.com>
2024-06-21 17:42:56 -0500

    caso1.py

--- caso1.py
+++ caso1.py
@@ -1,40 +1,45 @@
-
 #En python: Implementar la segmentacion de una imagen utilizando el algoritmo de k-means clustering
 #pasos.
 #1.leer una imagen y convertirla a un espacio de color adecuado(por ejemplo, RGB a L*a*b)
 #2.aplicar el algoritmo de k-means para segmentar la imagen en k regiones
 #3.visualizar la imagen segmentada y comparar los resultados con la imagen original
-
+#Utilizar la libreria de scikit-learn para aplicar k-means clustering
 #Acá importamos la libreria de operncv para leer y mostrar 
 #la imagen
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from skimage.color import rgb2lab, lab2rgb
 
 def kmeans_segmentation(image_path, k=3):
-    #K=k-means y es como un medidor para cambiar al k-means
     # Paso 1: Leer la imagen y convertirla al espacio de color L*a*b
     image = cv2.imread(image_path)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image_lab = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)  
-    pixel_values = image_lab.reshape((-1, 3))
-    pixel_values = np.float32(pixel_values)
+    image_lab = rgb2lab(image_rgb)
+
+    # Obtener las dimensiones de la imagen
+    w, h, d = image_lab.shape
+
+    # Reshape the image to a 2D array of pixels
+    image_array = image_lab.reshape((w * h, d))
 
     # Paso 2: Aplicar el algoritmo de k-means para segmentar la imagen en k regiones
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
-    _, labels, centers = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-    centers = np.uint8(centers)
-    labels = labels.flatten()
-    segmented_image = centers[labels.flatten()]
-    segmented_image = segmented_image.reshape(image_lab.shape)
-    # Convertimos de L*a*b a RGB
-    segmented_image_rgb = cv2.cvtColor(segmented_image, cv2.COLOR_Lab2RGB)
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(image_array)
+    segmented_labels = kmeans.labels_
+    centers = kmeans.cluster_centers_
+
+    # Crear la imagen segmentada
+    segmented_image_lab = centers[segmented_labels].reshape(image_lab.shape)
+
+    # Convertir la imagen segmentada de L*a*b a RGB
+    segmented_image_rgb = lab2rgb(segmented_image_lab)
 
     return image_rgb, segmented_image_rgb
 
 def main():
-    # Llamaamos a la imagen
-    image_path = 'ara.jpg'  # Reemplaza con la ruta a tu imagen
+    # Ruta de la imagen
+    image_path = 'imagen.jpg'  # Reemplaza con la ruta a tu imagen
 
     # Número de clusters
     k = 3
@@ -51,13 +56,12 @@ def main():
 
     plt.subplot(1, 2, 2)
     plt.imshow(segmented_image)
-    plt.title(f'Segmentacion en k Clusters')
+    plt.title(f'Segmentacion en k clusters')
     plt.axis('off')
 
     plt.show()
 
-if __name__ == "__main__":
-    main()
-
+if _name_ == "_main_":
+    main()        
 
 
